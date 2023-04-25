6732822e3390338bc3decdcec350baca9b15819f
mogfull7777 <anjunyoung94@gmail.com>
2023-04-25 22:19:00 +0900

    start showping #7

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
 const Login = () => {
+
+    // 1. 각 항목에 상수로 useState를 생성해준다.
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginSubmitHendle = async (e) => {
+        e.preventDefault()
+
+        try {
+
+            const userInput = {
+                email : email,
+                password : password
+            }
+
+            console.log(userInput)
+
+
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
     return (
         <Container className={"mt-5"}>
             <Row className={"justify-content-center"}>
                 <Col xs={12} md={6}>
                     <h1>LOGIN</h1>
-                    <Form className={"mt-4"}>
+                    <Form className={"mt-4"} onSubmit={loginSubmitHendle}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                 // value 값 정리
+                                value={email}
+                                 // onChange 중요
+                                onChange={e => setEmail(e.target.value)}
+                            />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
                             <Form.Check type="checkbox" label="Check me out" />
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,22 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
 const SignUp = () => {
+
+    const [username, setUsername] = useState("");
+    const [email, setemail] = useState("");
+    const [password, setPasswoed] = useState("");
+    const [confirmPassword, setconfirmPasswoed] = useState("");
+
+    const signupSubmitheandle = async (e) => {
+        e.preventDefault()
+
+        try {
+
+
+            // 2번째 = 비밀번호 확인
+            if (password !== confirmPassword) {
+                alert("password not match")
+            }
+
+
+            // 1번째
+            const userInput = {
+                username : username,
+                email : email,
+                password : password
+            }
+
+            console.log(userInput)
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    // console.log(username)
+
     return (
         <Container style={{marginTop : "5rem"}}>
             <Row className={"justify-content-md-center"}>
                 <Col xs={12} md={6}>
                     <h1>Sign Up</h1>
-                    <Form className={"mt-4"}>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form className={"mt-4"} onSubmit={signupSubmitheandle}>
+                        <Form.Group className="mb-3" controlId="formBasicusername">
                             <Form.Label>User name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter username" />
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter username"
+                                value={username}
+                                onChange={e => setUsername(e.target.value)}
+                            />
 
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={e => setemail(e.target.value)}
+                            />
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
@@ -24,8 +68,24 @@ const SignUp = () => {
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={e => setPasswoed(e.target.value)}
+                            />
                         </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Confirm Password"
+                                value={confirmPassword}
+                                onChange={e => setconfirmPasswoed(e.target.value)}
+                            />
+                        </Form.Group>
+
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
                         </Form.Group>
 
