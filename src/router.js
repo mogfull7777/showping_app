795ed26adc732b513ceedc51901388d97baabadd
mogfull7777 <anjunyoung94@gmail.com>
2023-04-25 21:01:28 +0900

    start showping #2

---
+++ src/router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {createBrowserRouter} from "react-router-dom";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+const Router = createBrowserRouter([
+    {
+        path : "/",
+        element : <Main />
+    },{
+        path : "/login",
+        element : <Login />
+    },{
+        path : "/signup",
+        element : <SignUp />
+    }
+])
+
+export default Router;
