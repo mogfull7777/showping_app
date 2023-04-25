95ea4fc623e675a75260815bd757d02f1de70219
mogfull7777 <anjunyoung94@gmail.com>
2023-04-25 21:46:58 +0900

    start showping #6

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
 const Login = () => {
     return (
-        <div>
-            <h1>this is login</h1>
-        </div>
+        <Container className={"mt-5"}>
+            <Row className={"justify-content-center"}>
+                <Col xs={12} md={6}>
+                    <h1>LOGIN</h1>
+                    <Form className={"mt-4"}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="success" type="submit">
+                            Login
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
